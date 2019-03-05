d7ff5036ef23f72a8a894fbf70c33cd76a44712d
Amir Kamankesh <A.KamanKesh@persianswitch.net>
2019-03-05 11:23:03 +0330

    new service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,94 @@
+const CACHE_NAME = 'asanpardakht-gardeshgari';
+
+/*
+    ***cache black list***
+    api and libraries than you wont be chached by service worker
+ */
+const CACHE_BLACKLIST = [
+    'http://2mrdl.mocklab.io'
+];
+
+/*
+    *** urls to cache ***
+    resources want to cache by service worker
+ */
+let URLS_TO_CACHE = [
+    '/',
+    '/index.html',
+    'js/app.js',
+    '/images/apple-touch-icon.png',
+    '/images/apple-touch-icon-152x152.png',
+    '/images/apple-touch-icon-180x180.png',
+    '/images/apple-touch-icon-120x120.png',
+    '/css/app.css'
+];
+
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    )
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then(function(response) {
+                // Check if we received a valid response
+                if (!response || response.status !== 200) {
+                    return response;
+                }
+
+                if (!isInBlackList(event)) {
+
+                    // to clone it so we have two streams.
+                    const responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+
+                }
+
+                return response;
+            });
+        })
+    );
+});
+
+function isInBlackList(event) {
+    CACHE_BLACKLIST.forEach(function (url) {
+        if (event.request.url.startsWith(url)) {
+            return true;
+        }
+    })
+}
+
+self.addEventListener('activate', function(event) {
+
+    const cacheWhitelist = [];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
 /*self.addEventListener('install', function (event) {
     console.log('SW installed');
     event.waitUntil(
@@ -36,7 +127,7 @@ self.addEventListener('fetch', function (event) {
 
 // const prechaches = self.__precacheManifest;
 
-let urls = [
+/*let urls = [
     '/',
     '/index.html',
     'js/app.js',
@@ -45,16 +136,16 @@ let urls = [
     '/images/apple-touch-icon-180x180.png',
     '/images/apple-touch-icon-120x120.png',
     '/css/app.css'
-];
+];*/
 
 /*prechaches.forEach(prechache => {
     urls.push(prechache.url);
 });*/
 
-const RUNTIME = 'test-worker';
+// const RUNTIME = 'test-worker';
 
 
-self.addEventListener('install', function(event) {
+/*self.addEventListener('install', function(event) {
     // Perform install steps
     console.log('installing sw');
     event.waitUntil(
@@ -71,12 +162,12 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', event => {
     self.clients.claim();
-});
+});*/
 
 // The fetch handler serves responses for same-origin resources from a cache.
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
-self.addEventListener('fetch', event => {
+/*self.addEventListener('fetch', event => {
     // The fetch handler serves responses for same-origin resources from a cache.
     // If no response is found, it populates the runtime cache with the response
     // from the network before returning it to the page.
@@ -112,3 +203,4 @@ self.addEventListener('fetch', event => {
         });
     }
 });
+*/
